5f977fb4a696fc0091762b651955eefb670cd146
kazumi_watanabe <kazumi_watanabe@lockon.co.jp>
2019-07-24 19:24:57 +0900

    権利表記の変更

--- Entity/CouponOrder.php
+++ Entity/CouponOrder.php
@@ -2,7 +2,7 @@
 /*
  * This file is part of the Coupon plugin
  *
- * Copyright (C) 2016 LOCKON CO.,LTD. All Rights Reserved.
+ * Copyright(c) EC-CUBE CO.,LTD. All Rights Reserved.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
